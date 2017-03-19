53b41b543662344512d420939a26b4607200e507
MinonHeart <git@hub.moe>
2017-03-19 21:12:14 +0800

    Add: global

---
+++ source/scripts/global/register.js
@@ -0,0 +1,49 @@
+/**
+ * Register
+ */
+Weibo.acceptType = {
+    "image/jpeg": {
+        type: ".jpg",
+        typo: ".jpg",
+    },
+    "image/png": {
+        type: ".png",
+        typo: ".jpg",
+    },
+    "image/gif": {
+        type: ".gif",
+        typo: ".gif",
+    },
+};
+
+Weibo.imagePostface = Object.values(Weibo.acceptType).map(item => item.type.slice(1)).join("|");
+
+Weibo.distinctProp = {
+    property: 2,
+    caption: "Weibo_Chrome",
+    description: "ImUfrNWhuFTTOXASFgdCVVv2ZUIquXrKjqiey2r95Kqudh6sjaBUWFdcwtlGEX2w", // 64 位特征码
+    answer: "",
+    question: "",
+    album_id: "",
+};
+
+Weibo.urlPrefix = [
+    "wx1", "wx2", "wx3", "wx4",
+    "ws1", "ws2", "ws3", "ws4",
+];
+
+Weibo.rootZone = ".sinaimg.cn";
+
+Weibo.startConfig = {
+    scheme: {
+        1: "http://",
+        2: "https://",
+        3: "//",
+    },
+    clipSize: {
+        1: "large",
+        2: "mw690",
+        3: "thumbnail",
+        4: "",
+    },
+};
