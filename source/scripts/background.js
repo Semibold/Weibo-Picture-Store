9102034366f4d0c5a3ea5c3fe8a35b52a454cf40
MinonHeart <git@hub.moe>
2017-03-24 11:13:12 +0800

    Update: -

--- source/scripts/background.js
+++ source/scripts/background.js
@@ -44,16 +44,31 @@ const uploadMenuEntryId = chrome.contextMenus.create({
     onclick: (obj, tab) => {
         if (Utils.checkImageURL(obj.srcUrl, true)) {
             Utils.fetchImage(obj.srcUrl)
-                .then(blob => Weibo.readFile([blob]))
-                .then(result => Weibo.fileUpload(result))
-                .then(result => result[0] && transformRaw(result[0]))
                 .catch(reason => {
-                    chrome.notifications.create({
+                    chrome.notifications.create(notifyId, {
                         type: "basic",
                         iconUrl: chrome.i18n.getMessage("64"),
                         title: chrome.i18n.getMessage("warn_title"),
                         message: chrome.i18n.getMessage("get_image_url_fail"),
                     });
+                    return Promise.reject();
+                })
+                .then(blob => Weibo.readFile([blob]))
+                .then(result => Weibo.filePurity(result))
+                .then(result => Weibo.fileUpload(result))
+                .then(result => {
+                    if (result[0]) {
+                        let url = `https://${Weibo.urlPrefix[0] + Weibo.rootZone}/large/${result[0].pid + Weibo.acceptType[result[0].file.type].typo}`;
+                        Utils.writeToClipboard(url, () => {
+                            chrome.notifications.create(notifyId, {
+                                type: "basic",
+                                iconUrl: chrome.i18n.getMessage("64"),
+                                title: chrome.i18n.getMessage("info_title"),
+                                message: chrome.i18n.getMessage("copy_to_clipboard"),
+                                contextMessage: chrome.i18n.getMessage("copy_to_clipboard_hinter"),
+                            });
+                        });                        
+                    }
                 });
         } else {
             chrome.notifications.create(notifyId, {
@@ -65,28 +80,6 @@ const uploadMenuEntryId = chrome.contextMenus.create({
         }
     },
 }, () => chrome.runtime.lastError && console.warn(chrome.runtime.lastError));
-
-const transformRaw = raw => {
-    let url = `https://${Weibo.urlPrefix[0] + Weibo.rootZone}/large/${raw.pid + Weibo.acceptType[raw.fileType].typo}`;
-    let span = document.createElement("span");
-    let spanRange = document.createRange();
-    let selection = document.getSelection();
-
-    span.textContent = url;
-    document.body.append(span);
-    spanRange.selectNodeContents(span);
-    selection.removeAllRanges();
-    selection.addRange(spanRange);
-    document.execCommand("copy");
-    span.remove();
-    chrome.notifications.create(notifyId, {
-        type: "basic",
-        iconUrl: chrome.i18n.getMessage("64"),
-        title: chrome.i18n.getMessage("info_title"),
-        message: chrome.i18n.getMessage("copy_to_clipboard"),
-        contextMessage: chrome.i18n.getMessage("copy_to_clipboard_hinter"),
-    });
-};
 
 
 chrome.webRequest.onBeforeSendHeaders.addListener(details => {
