713f61601bd4c57dcdd7ce0cf0253a10528b0bfa
MinonHeart <git@hub.moe>
2017-05-09 13:53:09 +0800

    Update: popup

--- source/scripts/popup/buildItem.js
+++ source/scripts/popup/buildItem.js
@@ -6,16 +6,10 @@ class BuildItem {
     constructor(item) {
         this.item = item;
         this.duplex = null;
+        this.section = null;
         this.objectURL = null;
         this.domNodes = {};
         this.decorator();
-
-        return {
-            section: this.domNodes.section,
-            repaint: this.repaint.bind(this),
-            destroy: this.destroy.bind(this),
-            domNodes: this.domNodes,
-        };
     }
 
     decorator() {
@@ -25,37 +19,35 @@ class BuildItem {
 
     createItem() {
         let image = new Image();
-        let template = document.querySelector("#template");
-        let fragment = document.importNode(template.content, true);
+        let fragment = BuildItem.importNode();
 
-        this.domNodes.section = fragment.querySelector("section");
-        this.domNodes.imageHolder = this.domNodes.section.querySelector(".image-holder");
-        this.domNodes.inputURL = this.domNodes.section.querySelector(".type-url input");
-        this.domNodes.inputHTML = this.domNodes.section.querySelector(".type-html input");
-        this.domNodes.inputUBB = this.domNodes.section.querySelector(".type-ubb input");
-        this.domNodes.inputMarkdown = this.domNodes.section.querySelector(".type-markdown input");
+        this.section = fragment.querySelector("section");
+        this.domNodes.imageHolder = this.section.querySelector(".image-holder");
+        this.domNodes.inputURL = this.section.querySelector(".type-1 input");
+        this.domNodes.inputHTML = this.section.querySelector(".type-2 input");
+        this.domNodes.inputUBB = this.section.querySelector(".type-3 input");
+        this.domNodes.inputMarkdown = this.section.querySelector(".type-4 input");
 
-        if (this.repaint(this.item)) {
+        if (this.repaint()) {
             if (this.item.objectURL) {
-                this.objectURL = this.item.objectURL;
-                image.src = this.objectURL;
+                this.objectURL = image.src = this.item.objectURL;
                 this.domNodes.imageHolder.append(image);
             }
         }
 
-        this.domNodes.section.dataset.guid = this.item.guid;
+        this.section.dataset.guid = this.item.guid;
     }
 
     buildEvent() {
-        this.duplex = new BuildEvent(this.domNodes.section);
+        this.duplex = new BuildEvent(this.section);
     }
 
-    repaint(item) {
-        if (item && item.URL) {
-            this.domNodes.inputURL.value = item.URL;
-            this.domNodes.inputHTML.value = item.HTML;
-            this.domNodes.inputUBB.value = item.UBB;
-            this.domNodes.inputMarkdown.value = item.Markdown;
+    repaint() {
+        if (this.item && this.item.URL) {
+            this.domNodes.inputURL.value = this.item.URL;
+            this.domNodes.inputHTML.value = this.item.HTML;
+            this.domNodes.inputUBB.value = this.item.UBB;
+            this.domNodes.inputMarkdown.value = this.item.Markdown;
             return true;
         } else {
             return false;
@@ -64,8 +56,44 @@ class BuildItem {
 
     destroy() {
         this.duplex.destroy();
-        this.domNodes.section.remove();
+        this.section.remove();
         this.objectURL && URL.revokeObjectURL(this.objectURL);
+    }
+
+    static importNode() {
+        let html = `
+            <section>
+                <div class="holder-wrapper">
+                    <div class="image-holder" title="上传图片到微博相册"></div>
+                </div>
+                <div class="table-wrapper">
+                    <table width="100%">
+                        <tbody>
+                            <tr class="type-1">
+                                <td><span class="title">URL</span></td>
+                                <td><input type="text" readonly spellcheck="false" placeholder="Uniform Resource Locator"></td>
+                                <td><a class="button-copy" data-type="URL">Copy</a></td>
+                            </tr>
+                            <tr class="type-2">
+                                <td><span class="title">HTML</span></td>
+                                <td><input type="text" readonly spellcheck="false" placeholder="HyperText Markup Language"></td>
+                                <td><a class="button-copy" data-type="HTML">Copy</a></td>
+                            </tr>
+                            <tr class="type-3">
+                                <td><span class="title">UBB</span></td>
+                                <td><input type="text" readonly spellcheck="false" placeholder="Ultimate Bulletin Board"></td>
+                                <td><a class="button-copy" data-type="UBB">Copy</a></td>
+                            </tr>
+                            <tr class="type-4">
+                                <td><span class="title">Markdown</span></td>
+                                <td><input type="text" readonly spellcheck="false" placeholder="Markdown"></td>
+                                <td><a class="button-copy" data-type="Markdown">Copy</a></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </section>`;
+        return Utils.parseHTML(html);
     }
 
 }
