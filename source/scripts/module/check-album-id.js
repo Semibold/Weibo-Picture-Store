d661b1c221eb70ffbb648fbf884e5dba29debb0b
MinonHeart <git@hub.moe>
2017-10-15 16:34:13 +0800

    Fixed: usage of Utils.singleton. Close #31

--- source/scripts/module/check-album-id.js
+++ source/scripts/module/check-album-id.js
@@ -2,44 +2,39 @@
 
     const overflow = 100;
     const url = "http://photo.weibo.com/albums/get_all";
+    const checkAlbumIdRequest = () => {
+        return Utils.fetch(Utils.buildURL(url, {page: 1, count: overflow})).then(response => {
+            return response.ok ? response.json() : Promise.reject(response.status);
+        }).then(json => {
+            if (json && json.result) {
+                const albumInfo = {
+                    counter: 0,
+                    albumId: null,
+                };
 
-    Weibo.checkAlbumId = () => {
-        const page = 1;
-        const count = overflow;
-        const request = () => {
-            return Utils.fetch(Utils.buildURL(url, {page, count})).then(response => {
-                return response.ok ? response.json() : Promise.reject(response.status);
-            }).then(json => {
-                if (json && json.result) {
-                    const albumInfo = {
-                        counter: 0,
-                        albumId: null,
-                    };
-
-                    for (const item of json.data.album_list) {
-                        albumInfo.counter++;
-                        if (item.description === Weibo.distinctProp.description) {
-                            albumInfo.albumId = item.album_id.toString();
-                            break;
-                        }
+                for (const item of json.data.album_list) {
+                    albumInfo.counter++;
+                    if (item.description === Weibo.distinctProp.description) {
+                        albumInfo.albumId = item.album_id.toString();
+                        break;
                     }
-
-                    if (albumInfo.albumId) {
-                        return Promise.resolve({
-                            albumId: albumInfo.albumId,
-                        });
-                    } else {
-                        return Promise.reject({
-                            canCreateNewAlbum: albumInfo.counter < overflow,
-                        });
-                    }
-                } else {
-                    return Promise.reject("Invalid Data");
                 }
-            });
-        };
 
-        return Utils.singleton(request);
+                if (albumInfo.albumId) {
+                    return Promise.resolve({
+                        albumId: albumInfo.albumId,
+                    });
+                } else {
+                    return Promise.reject({
+                        canCreateNewAlbum: albumInfo.counter < overflow,
+                    });
+                }
+            } else {
+                return Promise.reject("Invalid Data");
+            }
+        });
     };
+
+    Weibo.checkAlbumId = () => Utils.singleton(checkAlbumIdRequest);
 
 }
--- source/scripts/module/set-status.js
+++ source/scripts/module/set-status.js
@@ -3,43 +3,40 @@
     const doneCode = "100000";
     const iframeId = `iframe-${Utils.randomString(6)}`;
     const url = "http://weibo.com/aj/onoff/setstatus";
-
-    Weibo.setStatus = () => {
+    const setStatusRequest = () => {
         const method = "POST";
         const body = Utils.createSearchParams({sid: 0, state: 0});
-        const request = () => {
-            return Utils.fetch(url, {method, body}).then(response => {
-                if (response.ok) {
-                    if (response.redirected) {
-                        const iframe = document.getElementById(iframeId) || document.createElement("iframe");
-                        const promise = new Promise((resolve, reject) => {
-                            iframe.onload = e => resolve();
-                            iframe.onerror = e => reject(); // Useless
-                        });
-                        iframe.id = iframeId;
-                        iframe.src = response.url;
-                        document.body.append(iframe);
-                        return promise;
-                    } else {
-                        return Promise.reject(response.redirected);
-                    }
-                } else {
-                    return Promise.reject(response.status);
-                }
-            }).then(result => {
-                return Promise.resolve(Weibo.getStatus(true));
-            }).catch(reason => {
-                return Promise.reject(Weibo.getStatus(true));
-            });
-        };
-
         return Weibo.getStatus().then(json => {
             if (json.login) {
                 return Promise.reject(json);
             } else {
-                return Utils.singleton(request);
+                return Utils.fetch(url, {method, body}).then(response => {
+                    if (response.ok) {
+                        if (response.redirected) {
+                            const iframe = document.getElementById(iframeId) || document.createElement("iframe");
+                            const promise = new Promise((resolve, reject) => {
+                                iframe.onload = e => resolve();
+                                iframe.onerror = e => reject(); // Useless
+                            });
+                            iframe.id = iframeId;
+                            iframe.src = response.url;
+                            document.body.append(iframe);
+                            return promise;
+                        } else {
+                            return Promise.reject(response.redirected);
+                        }
+                    } else {
+                        return Promise.reject(response.status);
+                    }
+                }).then(result => {
+                    return Promise.resolve(Weibo.getStatus(true));
+                }).catch(reason => {
+                    return Promise.reject(Weibo.getStatus(true));
+                });
             }
         });
     };
+
+    Weibo.setStatus = () => Utils.singleton(setStatusRequest);
 
 }
