f9179d2eaac3db74a230954eb2ebe28817837edf
L&H <abc@hub.moe>
2018-03-30 18:41:36 +0800

    add something

---
+++ source/scripts/sharre/config.js
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2018 The Weibo-Picture-Store Authors. All rights reserved.
+ * Use of this source code is governed by a MIT-style license that can be
+ * found in the LICENSE file.
+ */
+
+/**
+ * @readonly
+ */
+export class Config {
+
+  /**
+   * @enum
+   * @desc Storage Service Provider Type
+   */
+  static get sspType() {
+    return new Set([
+      "weibo.com",
+      "tencent.com",
+      "qiniu.com",
+      "aliyun.com",
+      "upyun.com",
+    ]);
+  }
+
+}
