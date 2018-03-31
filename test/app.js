4dbe9550a70e64a4e40f6ee96be7c834dec446f9
L&H <abc@hub.moe>
2018-03-31 18:10:30 +0800

    add base64 and hash-hmac test file

---
+++ test/app.js
@@ -0,0 +1,8 @@
+/*
+ * Copyright (c) 2018 The Weibo-Picture-Store Authors. All rights reserved.
+ * Use of this source code is governed by a MIT-style license that can be
+ * found in the LICENSE file.
+ */
+
+import "./scripts/hash-hmac.js";
+import "./scripts/base64.js";
