804a707f42a528388b8e84ae6fbff2fb57bf6d33
L&H <abc@hub.moe>
2018-04-11 18:32:10 +0800

    Update: -

--- source/scripts/background/fetch-blob.js
+++ source/scripts/background/fetch-blob.js
@@ -5,7 +5,7 @@
  */
 
 import {Utils} from "../sharre/utils.js";
-import {FileProgress, FP_ACTION_DOWNLOAD} from "./file-progress.js";
+import {FileProgress} from "./file-progress.js";
 
 const failedId = Utils.randomString(16);
 
@@ -16,7 +16,7 @@ const failedId = Utils.randomString(16);
  */
 export async function fetchBlob(url) {
     const delayInfo = {interval: 500, requestId: null};
-    const progress = new FileProgress(FP_ACTION_DOWNLOAD);
+    const progress = new FileProgress(FileProgress.ACTION_DOWNLOAD);
 
     progress.padding(1);
     delayInfo.requestId = setTimeout(() => progress.trigger(), delayInfo.interval);
