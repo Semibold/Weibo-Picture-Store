48b42e8a1d8bb90594db8b7a327f5fe9a9adfc40
L&H <abc@hub.moe>
2018-04-11 15:22:50 +0800

    Add qcloud-storage.js

---
+++ source/scripts/plugin/constant.js
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2018 The Weibo-Picture-Store Authors. All rights reserved.
+ * Use of this source code is governed by a MIT-style license that can be
+ * found in the LICENSE file.
+ */
+
+/**
+ * Canvas 画布宽或高的最大值
+ */
+export const MAXIMUM_EDGE = 2 ** 15 - 1;
+
+export const TRACKER_ID = "since-20170309";
