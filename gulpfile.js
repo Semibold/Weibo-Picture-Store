a1f7ccf6423f803696887cdb4d67e39fef83bca1
L&H <abc@hub.moe>
2018-07-05 16:31:06 +0800

    Update: -

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,10 @@
-const fs = require("fs");
 const gulp = require("gulp");
 const gulp_zip = require("gulp-zip");
 const gulp_json_modify = require("gulp-json-modify");
 const merge_stream = require("merge-stream");
+const pkg = require("./package.json");
 
 gulp.task("bundle", function () {
-    const pkg = JSON.parse(fs.readFileSync("./package.json", {encoding: "utf8"}));
     const others = gulp.src(["src/**", "!src/manifest.json"]);
     const manifest = gulp.src("src/manifest.json")
         .pipe(gulp_json_modify({
