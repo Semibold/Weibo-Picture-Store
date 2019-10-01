26bf19ae7431f148dd491363e5400282008e7ea9
L&H <abc@hub.moe>
2019-10-01 12:18:37 +0800

    Update project workflow

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ const gulp_json_modify = require("gulp-json-modify");
 const merge_stream = require("merge-stream");
 const pkg = require("./package.json");
 
-gulp.task("bundle", function() {
+gulp.task("bundle:chrome", function() {
     const others = gulp.src(["src/**", "!src/manifest.json"]);
     const manifest = gulp.src("src/manifest.json").pipe(
         gulp_json_modify({
@@ -15,5 +15,32 @@ gulp.task("bundle", function() {
 
     return merge_stream(manifest, others)
         .pipe(gulp_zip("bundle.zip"))
-        .pipe(gulp.dest("dist"));
+        .pipe(gulp.dest("dist/chrome"));
+});
+
+gulp.task("bundle:firefox", function() {
+    const others = gulp.src(["src/**", "!src/manifest.json"]);
+    const manifest = gulp
+        .src("src/manifest.json")
+        .pipe(
+            gulp_json_modify({
+                key: "version",
+                value: pkg.version,
+            }),
+        )
+        .pipe(
+            gulp_json_modify({
+                key: "browser_specific_settings",
+                value: {
+                    gecko: {
+                        id: "weibo-picture-store@ext.hub.moe",
+                        strict_min_version: "66.0",
+                    },
+                },
+            }),
+        );
+
+    return merge_stream(manifest, others)
+        .pipe(gulp_zip("bundle.zip"))
+        .pipe(gulp.dest("dist/firefox"));
 });
