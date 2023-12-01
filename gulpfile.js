55fdcfc40812407048f5f46e9e4c5c58fb52e516
L&H <abc@hub.moe>
2023-12-01 11:18:26 +0800

    chore: update build workflow

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,12 @@
 const gulp = require("gulp");
 const gulp_zip = require("gulp-zip");
+const gulp_rename = require('gulp-rename');
 const gulp_json_modify = require("gulp-json-modify");
 const merge_stream = require("merge-stream");
 const pkg = require("./package.json");
-const mft = require("./src/manifest.json");
 
-gulp.task("bundle:chrome", function() {
-    const others = gulp.src(["src/**", "!src/manifest.json"]);
+gulp.task("bundle:chrome", () => {
+    const others = gulp.src(["src/**", "!src/manifest.json", "!src/manifest.firefox.json", "!src/**/*.ts", "!src/**/*.js.map"]);
     const manifest = gulp.src("src/manifest.json").pipe(
         gulp_json_modify({
             key: "version",
@@ -19,35 +19,30 @@ gulp.task("bundle:chrome", function() {
         .pipe(gulp.dest("dist/chrome"));
 });
 
-gulp.task("bundle:firefox", function() {
-    const others = gulp.src(["src/**", "!src/manifest.json"]);
+gulp.task("bundle:firefox", () => {
+    const others = gulp.src(["src/**", "!src/manifest.json", "!src/manifest.firefox.json", "!src/**/*.ts", "!src/**/*.js.map"]);
     const manifest = gulp
-        .src("src/manifest.json")
+        .src("src/manifest.firefox.json")
         .pipe(
             gulp_json_modify({
                 key: "version",
                 value: pkg.version,
             }),
         )
-        .pipe(
-            gulp_json_modify({
-                key: "permissions",
-                value: ["menus"].concat(mft.permissions),
-            }),
-        )
-        .pipe(
-            gulp_json_modify({
-                key: "browser_specific_settings",
-                value: {
-                    gecko: {
-                        id: "weibo-picture-store@ext.hub.moe",
-                        strict_min_version: "68.0",
-                    },
-                },
-            }),
-        );
+        .pipe(gulp_rename('manifest.json'));
 
     return merge_stream(manifest, others)
         .pipe(gulp_zip("bundle.zip"))
         .pipe(gulp.dest("dist/firefox"));
+});
+
+/**
+ * Task for CI/CD
+ * Use git to revert changes if call it locally
+ */
+gulp.task('apply:manifest:firefox', () => {
+    return gulp
+        .src("src/manifest.firefox.json")
+        .pipe(gulp_rename('manifest.json'))
+        .pipe(gulp.dest("src"))
 });
